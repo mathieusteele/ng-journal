2a21dd5e2521fe885902bc61821060b9cca0849b
Mathieu Steele <matt@steeleagency.com>
2021-12-01 21:55:49 -0500

    add some component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JournalComponent } from './journal/journal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/journal', pathMatch: 'full' },
+  {
+    path: 'journal',
+    component: JournalComponent,
+    // children: [
+    //   { path: 'new', component: DocumentEditComponent },
+    //   {
+    //     path: ':id',
+    //     component: DocumentDetailComponent,
+    //   },
+    //   { path: ':id/edit', component: DocumentEditComponent },
+    // ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,16 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntryComponent } from './journal/entry/entry.component';
 import { JournalComponent } from './journal/journal.component';
+import { EntryListComponent } from './journal/entry-list/entry-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntryComponent,
-    JournalComponent
+    JournalComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
